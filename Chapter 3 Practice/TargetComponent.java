f16a440b5100c1731477567f67c5fe898d6137b3
dwlim12 <dwlim@NN121-HA-01.HS.sd203.org>
2015-09-21 10:29:14 -0500

    Trying to draw stuff

---
+++ Chapter 3 Practice/TargetComponent.java
@@ -0,0 +1,13 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import javax.swing.JComponent;
+
+public class TargetComponent extends JComponent
+{
+    public void paintComponent(Graphics g)
+    {
+        Graphics2D g2 = (Graphics2D) g;
+        Circle c1 = draw(Shape circle);
+        
+    }
+}
